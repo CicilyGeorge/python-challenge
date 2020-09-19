2effacb32769bc9b89248e5d008cda1267234903
Cicily George <69083251+CicilyGeorge@users.noreply.github.com>
2020-09-19 18:21:00 +0800

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,9 +4,14 @@ import csv
 input_file = os.path.join("Resources","election_data.csv")
 output_file = os.path.join("analysis","poll_analysis.txt")
 
+# initializing empty lists
 voter_id = []
 candidate_list = []
-poll = {}
+
+# initializing empty dictionary
+poll = {} 
+
+# reading the input file and storing needed columns as lists
 with open(input_file) as poll_data:
     reader = csv.reader(poll_data, delimiter = ',')
     header = next(reader)
@@ -15,28 +20,40 @@ with open(input_file) as poll_data:
         voter_id.append(row[0])
         candidate_list.append(row[2])
 
-
+# calculating frequency of occurance of each candidate in candidate_list to poll dictionary
 for candidate in candidate_list:
     if (candidate in poll):
         poll[candidate] += 1
     else:
         poll[candidate] = 1
 
+# calculating total votes
 total_votes = len(voter_id)   
+
+#finding winner by getting the key paired with the max value in poll directory
 winner = max(poll, key=poll.get)
 
+# formatting the output string
 output = (
     f"\nElection Results\n"
     f"----------------------------\n"
       
     f"Total Votes: {total_votes}\n"
     f"----------------------------\n" )
+
+# loop through the keys and values in the dictionary and appending them to the output string
 for key, value in poll.items():
     percent_vote = "{:.3f}".format(value / total_votes * 100)         
     output += (f"{key} : {percent_vote}% ({value})\n" )
+
+# outside the loop appending final result to the output string
 output += (f"----------------------------\n"
     f"Winner: {winner}\n"
     f"----------------------------\n" )   
+
+#print output on terminal
 print(output)
+
+#write output to text file
 with open(output_file, "w") as txt_file:
-    txt_file.write(output)   
+    txt_file.write(output) 
